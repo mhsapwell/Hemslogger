9285db28f2f1cd26312478df289972cc1a8afa98
mhsapwell <mhsapwell@gmail.com>
2025-10-19 14:16:40 +0100

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,52 @@
-const CACHE = 'hems-cache-v1';
-const ASSETS = ['./','index.html','manifest.webmanifest','sw.js','icons/icon-192.png','icons/icon-512.png'];
+// sw.js (stable for Pages subpath)
+const CACHE = 'hems-cache-v142-safe1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.webmanifest',
+  './sw.js',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
 
-self.addEventListener('install', (e) => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
 });
 
-self.addEventListener('activate', (e) => {
-  e.waitUntil(
-    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    ).then(() => self.clients.claim())
   );
 });
 
-self.addEventListener('fetch', (e) => {
-  e.respondWith(
-    caches.match(e.request).then(r => 
-      r || fetch(e.request).then(res => {
+self.addEventListener('fetch', (event) => {
+  const req = event.request;
+  const isHTML = req.mode === 'navigate' ||
+                 req.destination === 'document' ||
+                 req.url.endsWith('/index.html');
+
+  if (isHTML) {
+    event.respondWith(
+      fetch(req).then(res => {
         const copy = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, copy));
+        caches.open(CACHE).then(c => c.put('./index.html', copy));
         return res;
-      }).catch(() => caches.match('index.html'))
-    )
+      }).catch(() => caches.match('./index.html'))
+    );
+    return;
+  }
+
+  event.respondWith(
+    caches.match(req).then(cached => {
+      if (cached) return cached;
+      return fetch(req).then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(req, copy));
+        return res;
+      });
+    })
   );
 });
